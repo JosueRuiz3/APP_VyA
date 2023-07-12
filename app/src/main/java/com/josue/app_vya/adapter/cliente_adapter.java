5816b381b5a9832450330dc73b32144ce35d0235
JosueRuiz3 <andyjosue123.jr@gmail.com>
2023-07-12 09:48:03 -0600

    Se eliminaron fragmentes innecesarios

--- app/src/main/java/com/josue/app_vya/adapter/cliente_adapter.java
+++ app/src/main/java/com/josue/app_vya/adapter/cliente_adapter.java
@@ -1,9 +1,7 @@
 package com.josue.app_vya.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,22 +9,17 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.josue.app_vya.CustomerDetailsActivity;
-import com.josue.app_vya.CustomerDetailsFragment;
-import com.josue.app_vya.DetailsActivity;
 import com.josue.app_vya.R;
 import com.josue.app_vya.model.cliente;
-import com.josue.app_vya.model.venta;
 
 public class cliente_adapter extends FirestoreRecyclerAdapter<cliente, cliente_adapter.ViewHolder> {
     private FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
